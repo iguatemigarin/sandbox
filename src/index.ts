0f4103357d96223b2b6dcaf9c204719dd6430dec
Iguatemi Garin <iguatemi@gmail.com>
2022-05-28 16:43:02 +0000

    Add sentry

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import * as Sentry from '@sentry/browser';
+import { BrowserTracing } from '@sentry/tracing';
 import { boot } from './app';
+
+Sentry.init({
+  dsn: 'https://89402cd2f8344ebfb6e7733b96764853@o214695.ingest.sentry.io/6449929',
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: Number(process.env.SENTRY_SAMPLE_RATE),
+});
 
 boot();
