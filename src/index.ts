0a1bcf6c2b3300e1eb71ceb4a8676ac33046cf01
Iguatemi Garin <iguatemi@gmail.com>
2022-05-28 16:50:04 +0000

    Better versioninf

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { boot } from './app';
 import project from '../package.json';
 
 Sentry.init({
-  release: `${project.name}-${project.version}`,
+  release: `${project.name}@${project.version}`,
   dsn: 'https://89402cd2f8344ebfb6e7733b96764853@o214695.ingest.sentry.io/6449929',
   integrations: [new BrowserTracing()],
   tracesSampleRate: Number(process.env.SENTRY_SAMPLE_RATE),
